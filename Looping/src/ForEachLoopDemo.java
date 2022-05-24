b609e19558cb81db8d512d551161a7f7663940c7
Rohith <Rohith@DESKTOP-7BERP6V>
2022-05-24 13:16:04 +0530

    first commit

---
+++ Looping/src/ForEachLoopDemo.java
@@ -0,0 +1,12 @@
+
+public class ForEachLoopDemo {
+
+		// TODO Auto-generated constructor stub
+		public static void main(String args[]){
+			int s[] = {18,25,28,29,30};
+			for (int i : s) {
+			System.out.println(i);
+			}
+			}
+			}
+
---
+++ Looping/src/WhileTest.java
@@ -0,0 +1,20 @@
+
+public class WhileTest {
+
+	public WhileTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=5;
+		while(i<=15)
+		{
+			System.out.println(i);
+			i=i+2;
+			
+		}
+
+	}
+
+}
