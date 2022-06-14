2f94fd057c7ba5dfcf531f5f45a7c35b0aa20f76
Rohith <Rohith@DESKTOP-7BERP6V>
2022-06-14 11:21:30 +0530

    commit

---
+++ HELLO/src/Loops/ForLoopCheckThePrimeNumber.java
@@ -0,0 +1,26 @@
+package Loops;
+import java.util.Scanner;
+public class ForLoopCheckThePrimeNumber {
+
+	public ForLoopCheckThePrimeNumber() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the value N");
+int n=sc.nextInt();
+boolean isprime= true;
+for(int i=2;i*i<=n;i++) {
+	if(n%i==0) {
+		isprime= false;
+		break;
+	}
+	}
+	if(n<2) isprime= false;
+	System.out.println("isprime" +isprime);
+}
+	}
+
+
