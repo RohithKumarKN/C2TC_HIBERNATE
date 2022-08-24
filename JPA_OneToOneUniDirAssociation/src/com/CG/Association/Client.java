ec87d865e8c2e55f59392a08b0a994008e9048ae
Rohith Kumar K N <106142690+RohithKumarKN@users.noreply.github.com>
2022-08-24 18:27:07 +0530

    Add files via upload

---
+++ JPA_OneToOneUniDirAssociation/src/com/CG/Association/Client.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.CG.Association;
+
+/**
+ * @author Rohith
+ *
+ */
+public class Client {
+
+	/**
+	 * 
+	 */
+	public Client() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
